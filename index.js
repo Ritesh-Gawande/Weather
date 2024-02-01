8a083570a9f8ae7339462adda801eea2e4451955
Ritesh Gawande <106462362+Ritesh-Gawande@users.noreply.github.com>
2024-02-01 20:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '847b659ba5a659ffbfa8703f0a1f422a';
+    const APIKey = 'API key';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
